e4d89d18a4cb38500d5601ad55aef2111d86dee2
kamalbakour <kamal.bak92@gmail.com>
2020-06-09 23:33:20 +0300

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,31 +1,43 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      callback: props.handleLogin,
+      username: undefined,
+      password: undefined
+    };
   }
 
+  handleChange = (e) => {
+    return this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    return (this.state.username && this.state.password) && this.state.callback({ username: this.state.username, password: this.state.password });
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <button type="submit">Log in</button>
         </div>
-      </form>
+      </form >
     );
   }
 }
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,44 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxChars: this.props.maxChars,
+      value: '',
+      counter: this.props.maxChars
+    };
+  }
 
-    this.state = {};
+  handleChange = (event) => {
+    const { key: eventKey, target: { value: eventValue } } = event;
+    let { countStateHolder, maxChars } = this.state;
+    return this.setState(
+      (prev) => {
+        return {
+          ...prev,
+          value: eventValue,
+          counter: maxChars - eventValue.length
+        }
+      }
+    )
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text"
+          name="message"
+          id="message"
+          value={this.state.value}
+          maxLength={this.state.maxChars}
+          onChange={this.handleChange}
+        //tabIndex="0"
+        />
+        <p>Only {this.state.counter} character left for you to use! Use your words wisely! and</p>
       </div>
     );
   }
 }
-
 export default TwitterMessage;
